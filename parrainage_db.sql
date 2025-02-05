87d3a53f6bffceb751e5583946cafc547ffcb0a2
AYMANE BR4 <118852754+AYMANE777@users.noreply.github.com>
2025-02-05 14:54:23 +0100

    Update parrainage_db.sql

--- parrainage_db.sql
+++ parrainage_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 11, 2022 at 01:27 PM
+-- Generation Time: Jun 01, 2025 at 01:27 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
